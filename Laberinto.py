ba6aefc4cfa207b4886151648dcd3a245ff7f658
migueliiin <91721552+migueliiin@users.noreply.github.com>
2021-12-07 17:33:24 +0100

    funcion mover hecha

--- Laberinto.py
+++ Laberinto.py
@@ -8,16 +8,37 @@ laberinto=  [
 
 # muro= ((0,1),(0,2),(0,3),(0,4),(1,1),(2,1),(2,3),(3,3),(4,0),(4,1),(4,2),(4,3))
 
-# 1=arriba 2=derecha 3=abajo 4=izquierda
+# 1=abajo 2=derecha 3=arriba 4=izquierda
 camino = (0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
 
+x=0
+y=0
+
+def mover(x,y,mov):
+    if(mov==1):
+        x=x+1
+    elif(mov==2):
+        y=y+1
+    elif(mov==3):
+        x=x-1
+    elif(mov==4):
+        y=y-1
+    else:
+        print("DEL 1 al 4")
+
+
 def main():
-    x=0
-    y=0
+
+
     for i in range(0, 5):
         for j in range(0, 5):
             print(i,j,"   ",laberinto[i][j])
         print()
+    
+    while(laberinto[x][y]=='s'):
+
+
+
 
 
 
