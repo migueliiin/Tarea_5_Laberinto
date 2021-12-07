eb6dc9c4943b2ea029fa56b5d4c8e9b7c943cff5
migueliiin <91721552+migueliiin@users.noreply.github.com>
2021-12-07 19:12:26 +0100

    Todo l hehco hasta las 19:12

--- Laberinto.py
+++ Laberinto.py
@@ -12,22 +12,28 @@ laberinto=  [
 # 1=abajo 2=derecha 3=arriba 4=izquierda
 camino = []
 
-def buscar_mover(x,y):
+def buscar_mover(x1,y1,ult):
     salir=False
     mov=1
     while(salir==False):
-        if(mov==1 and laberinto[x+1][y]==' ' and x+1<5 ):
+        if(mov==1 and ult != 3 and x1+1<5):
+            if(laberinto[x1+1][y1]==' '):
+                salir=True
+        elif(mov==2 and ult != 4 and y1+1<5):
+            if(laberinto[x1][y1+1]==' '==' '):
+                salir=True
+        elif(mov==3 and ult != 1 and x1-1>-1):
+            if(laberinto[x1-1][y1]==' '):
+                salir=True
+        elif(mov==4 and ult != 2 and y1-1>-1):
+            if(laberinto[x1][y1-1]==' '):
+                salir=True
+        elif(mov>4):
             salir=True
-        elif(mov==2 and laberinto[x][y+1]==' ' and y+1<5):
-            salir=True
-        elif(mov==3 and laberinto[x-1][y]==' ' and x-1>-1):
-            salir=True
-        elif(mov==4 and laberinto[x][y-1]==' ' and y-1>-1):
-            salir=True
-        else:
-            salir=True
-    if(mov in [1,2,3,4]):
-        return mov
+        mov = mov + 1
+    print("buscar ", mov-1)
+    if(mov-1 in [1,2,3,4]):
+        return mov-1
     else:
         return 0
 
@@ -55,20 +61,21 @@ def mover(x1,y1,mov):
 def main():
 
     salir = False
+    ult = 0
 
     while(salir == False):
-        mov=buscar_mover(x,y)
+        mov=buscar_mover(x,y,ult)
+        ult=mov
         print("-----",mov, " x =",x," y = ",y)
         if(mov in [1,2,3,4]):
             mover(x,y,mov)
         if(laberinto[x][y] == 's'):
             salir = True
-
-
         
     for i in range(0, 1):
         print(i,"   ",camino[i])
 
+    print("FFIINN------------------")
 
 
 if __name__ == "__main__":
